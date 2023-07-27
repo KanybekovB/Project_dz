36baa5d23b71ace77a7e5f90a7af2dd0a77832d4
Bektur Kanybekov <kanybekovbektur2007@gmail.com>
2023-07-27 16:55:53 +0600

    projectDz

--- js/lesson.js
+++ js/lesson.js
@@ -119,3 +119,112 @@ convert(eur, [usd, som, frank, yen, tenge], false);
 convert(frank, [usd, eur, som, yen, tenge], false);
 convert(yen, [usd, eur, frank, som, tenge], false);
 convert(tenge, [usd, eur, frank, yen, som], false);
+
+
+//* CARD SWITHCER
+
+const card = document.querySelector('.card');
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector('#btn-prev');
+let count = 1;
+
+
+
+const fecthCardById = (index) => {
+  fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error')
+      } else {
+        return response.json()
+      }
+    })
+    .then(data => {
+      updateCard(data)
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+}
+
+const updateCard = (data) => {
+  card.innerHTML = `
+    <p>${data.title}</p>
+    <p style="color: ${data.completed ? 'green': 'red'}">${data.completed}</p>
+    <span>${data.id}</span>
+  `
+  card.style.boxShadow = `0 0 60px ${data.completed ? 'green' : 'red'}`;
+  card.style.border = `1px solid ${data.completed ? 'green' : 'red'}`;
+}
+
+btnNext.onclick = () => {
+  if(count < 200) {
+    count++;
+  }else {
+    count = 1;
+  }
+  fecthCardById(count)
+}
+
+btnPrev.onclick = () => {
+  if(count > 1) {
+    count--;
+  }else {
+    count = 200;
+  }
+  fecthCardById(count)
+}
+
+fecthCardById(count)
+
+// btnNext.onclick = () => {
+//   count++;
+//   fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+//     .then(response => response.json())
+//     .then(data => {
+//       card.innerHTML = `
+//         <p>${data.title}</p>
+//         <p style="color: ${data.completed ? 'green': 'red'}">${data.completed}</p>
+//         <span>${data.id}</span>
+//       `
+//       console.log(data);
+//     })
+// }
+
+
+//* RANDOM CARD
+
+const randomCard = document.querySelector('.randomCard');
+let randomCount;
+
+const fecthRandomCard = (index) => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
+    .then(response => response.json())
+    .then(data => {
+      randomData(data)
+    })
+}
+
+const randomData = (data) => {
+  randomCard.innerHTML = `
+      <p>${data.title}</p>
+      <p>${data.body}</p>
+      <span>${data.id}</span>
+    `
+}
+const randomIndex = (length) => {
+  randomCount = Math.floor(Math.random() * length) + 1;
+  fecthRandomCard(randomCount)
+}
+
+randomIndex(100)
+
+setInterval(() => {
+  randomIndex(100)
+},4000)
+
+
+
+// const response = fetch('https://jsonplaceholder.typicode.com/posts/')
+// .then(response => response.json())
+// .then(data => console.log(data.length));
